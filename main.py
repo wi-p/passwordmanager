024ee061ed8fe87affd1a1a9d04b1f74d1dbacf2
Ale <worker.irish@gmail.com>
2026-03-06 21:29:09 -0300

    added deleted function on main.py file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDIconButton
 from kivy.metrics import dp
+from functools import partial
 
 class DBApp(DataBase):
     pass
@@ -15,8 +16,10 @@ class HomeScreen(MDScreen):
     def on_enter(self, *args):
         Clock.schedule_once(self.load_passwords)
     
-    def deletePassword(self, id ):
+    def deletePassword(self, id, instance ):
         DBApp('appdatabase').deleteItemTable('AppPassowrd', 'id', id)
+
+        self.ids.lstpwd.remove_widget(self.ids.id)
 
     def load_passwords(self, *args):
         values = DBApp('appdatabase').readTable('AppPassowrd', '*')
@@ -26,19 +29,23 @@ class HomeScreen(MDScreen):
 
         for value in values:
             box = MDBoxLayout()
+            box.id = str(value[0])
             box.size_hint_y = None
             box.height = dp(50)
             box.spacing = dp(10)
+            btn = MDIconButton(icon='trash-can', md_bg_color = 'red')
+            btn.bind(on_release = partial(self.deletePassword, value[0]))
 
             box.add_widget(MDLabel(text = str(value[0]), size_hint_x = None, width = dp(40)))
             box.add_widget(MDLabel(text = value[1]))
             box.add_widget(MDLabel(text = value[2]))
             box.add_widget(MDLabel(text = value[3]))
-            box.add_widget(MDIconButton(icon='trash-can', md_bg_color = 'red'))
+            box.add_widget(btn)
 
             self.ids.lstpwd.add_widget(
                 box
             )
+        
         
 
 class AddScreen(MDScreen):
